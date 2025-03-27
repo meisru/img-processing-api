463cc26d1190e4a5ef4904117330734b2589a105
Rudi <irodyme@gmail.com>
2025-03-27 04:30:25 +0300

    gitignore dist

--- dist/utilities/loggerMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var middleware = function (req, res, next) {
-  console.log(req.url, " was visited");
-  next();
-};
-exports.default = middleware;
